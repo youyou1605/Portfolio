95b839566164c39596b5d256da036943ed1ed6d8
youyou1605 <64137553+youyou1605@users.noreply.github.com>
2023-04-06 16:51:19 +0000

    [Fix]migration

--- db/migrate/20230214151819_create_relationships.rb
+++ db/migrate/20230214151819_create_relationships.rb
@@ -3,9 +3,9 @@ class CreateRelationships < ActiveRecord::Migration[6.1]
     create_table :relationships do |t|
       t.references :customer, foreign_key: true
       t.references :follower, foreign_key: { to_table: :customers }
+      t.index [:customer_id, :follower_id], unique: true
       t.timestamps
 
-      t.index [:customer_id, :follower_id], unique: true
     end
   end
 end
